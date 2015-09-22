d3bde4ad3b2d0f34d60cc36e7527ee2c82586a00
lowyjoe <liuyijiao1989@126.com>
2015-09-22 16:44:21 +0800

    添加 IE8+以上支持bootstrap和echarts

---
+++ WebContent/charts/EchartsUtil.js
@@ -0,0 +1,11 @@
+/**
+ * Echarts 通用类
+ */
+/*配置方法*/
+function requireConfig(){
+	require.config({
+	    paths:{ 
+	        echarts: '../plugins/echarts2.2.7'
+	    }
+	});
+}
---
+++ WebContent/charts/test.js
@@ -0,0 +1,49 @@
+/**
+ * test 界面生成 echarts js
+ */
+// 路径配置  develop 模式------正式项目中不使用
+/* require.config({ // 路径配置  其实就是把告诉esl.js相关的文件路径去哪里找
+            packages: [
+                {
+                    name: 'echarts',
+                    location: '../plugins/echarts2.2.7',//这里与目录有关，这个./src的意思就是到本文件的所在目录下的src目录下查找
+                    main: 'echarts'
+                },
+                {
+                    name: 'zrender',
+                    location: '../plugins/zrender',
+                    main: 'zrender'
+                }
+            ]
+        }); */
+// for echarts online home page
+function option0(){
+	 option = {
+            tooltip: {
+                show: true
+            },
+            legend: {
+                data:['销量']
+            },
+            xAxis : [
+                {
+                    type : 'category',
+                    data : ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+            series : [
+                {
+                    "name":"销量",
+                    "type":"bar",
+                    "data":[5, 20, 40, 10, 10, 20]
+                }
+            ]
+        };
+	return option;
+}
+
