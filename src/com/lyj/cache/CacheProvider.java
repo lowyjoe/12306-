b8c9ba3fa49a7962ef26ddbf8fdbeb20a84d9c64
liuyijiao <liuyijiao1989@126.com>
2015-10-28 15:51:00 +0800

    暂时不用

---
+++ src/com/lyj/cache/CacheProvider.java
@@ -0,0 +1,31 @@
+package com.lyj.cache;
+/**
+ * Support for pluggable caches.
+ * @author liudong
+ */
+public interface CacheProvider {
+
+	/**
+	 * Configure the cache
+	 *
+	 * @param regionName the name of the cache region
+	 * @param properties configuration settings
+	 * @throws CacheException
+	 */
+	public Cache buildCache(String regionName) throws CacheException;
+
+	/**
+	 * Callback to perform any necessary initialization of the underlying cache implementation
+	 * during SessionFactory construction.
+	 *
+	 * @param properties current configuration settings.
+	 */
+	public void start() throws CacheException;
+
+	/**
+	 * Callback to perform any necessary cleanup of the underlying cache implementation
+	 * during SessionFactory.close().
+	 */
+	public void stop();
+	
+}
---
+++ src/com/lyj/cache/ICacheInvoker.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.lyj.cache;
+
+/**
+ * 回调接口
+ * @author Winter Lau
+ */
+public interface ICacheInvoker {
+
+	public Object callback(Object...args) ; 
+	
+}
