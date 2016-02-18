f36b4c067258d1ccc703384cb1bbeb59c0e47b7a
liuyijiao <liuyijiao1989@126.com>
2016-02-18 10:20:15 +0800

    自动翻页

--- src/com/lyj/utils/Xiyouji.java
+++ src/com/lyj/utils/Xiyouji.java
@@ -22,25 +22,41 @@ public class Xiyouji {
 		 String url = "http://www.guoxue.com/minqingstory/ss/syj_0"+pageNum+".htm";
 		 Document doc = Jsoup.parse(new URL(url).openStream(), "GBK", url);
 		 char[] charText = doc.body().text().toCharArray();
+		 int charLength=charText.length;
 			Scanner sc = new Scanner(System.in);
 			System.out.println("《西游记》");
-			System.out.println("按enter开始。。。。。");
-			System.out.println("按q保存退出。。。。。");
-			System.out.println("按n下一页。。。。。");
-				 for (int i = saveI; i < charText.length; i++) {
-					if (i!=0&&i % 50 == 0) {
+			System.out.print("按enter开始。。。。。");
+			System.out.print("q保存退出。。。。。");
+			System.out.print("n下一页。。。。。");
+			System.out.print("p上一页。。。。。");
+				 for (int i = saveI; i <=charLength; i++) {
+					if ((i!=saveI)&&(i%50 == 0)&&(i!=charLength)) {
 						String nextStr=sc.nextLine();
+						//读一行保存一行
+						PropertiesUtil.setValueByKey("xiyouji.properties", "pageNum",pageNum+"");
+						PropertiesUtil.setValueByKey("xiyouji.properties", "i",(i-50)+"");
 					    if(nextStr.equals("q")){
 							System.out.println("保存退出");
 							PropertiesUtil.setValueByKey("xiyouji.properties", "pageNum",pageNum+"");
-							PropertiesUtil.setValueByKey("xiyouji.properties", "i",i+"");
+							PropertiesUtil.setValueByKey("xiyouji.properties", "i",(i-50)+"");
 							System.exit(0);
 						}else if(nextStr.equals("n")){
 							System.out.println("下一页");
 							pageNum=pageNum+1;
 							saveI=0;//i重置为0
 							readDoc(saveI, pageNum);
+						}else if(nextStr.equals("p")){
+							System.out.println("上一页");
+							pageNum=pageNum-1;
+							saveI=0;//i重置为0
+							readDoc(saveI, pageNum);
 						}
+					}
+					if(i==charLength){//如果是最后一个
+						System.out.println("下一页");
+						pageNum=pageNum+1;
+						saveI=0;//i重置为0
+						readDoc(saveI, pageNum);
 					}
 					System.out.print(charText[i]);
 				} 
